f11f052cdefe1e3e47c66743044ee078512bf7b3
renatmirzoev <renat.mirzoev@iute.com>
2025-09-25 14:45:44 +0300

    Initial commit

---
+++ src/test/java/com/renatmirzoev/pethealthportal/PetHealthPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renatmirzoev.pethealthportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetHealthPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
